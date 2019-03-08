f7d7fe1503983f6a754114c85e8c9f232d6ca6c7
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-03-07 20:40:49 -0700

    initializing folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Baja");
+
+SELECT * FROM burgers_db.burgers;
