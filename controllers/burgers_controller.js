924ec3c9a09dd53b864a7d76d55e691a5d53a4f5
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-03-08 20:46:57 -0700

    fix burger_control routes, html now working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,3 +5,37 @@ var router = express.Router();
 //Import the model (burger.js) to use its database functions
 var burger = require("../models/burger.js");
 
+//set default route for pulling initial list from DB
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      res.render("index", hbsObject);
+    });
+  });
+  
+  //Post new burger to list
+  router.post("/burgers", function(req, res) {
+    burger.insertOne([
+      "burger_name"
+    ], [
+      req.body.burger_name
+    ], function(data) {
+      res.redirect("/");
+    });
+  });
+  
+  //update the burger from not devoured to devoured 
+  router.put("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.updateOne({
+      devoured: true
+    }, condition, function(data) {
+      res.redirect("/");
+    });
+  });
+  
+  // Export routes for server.js
+  module.exports = router;
